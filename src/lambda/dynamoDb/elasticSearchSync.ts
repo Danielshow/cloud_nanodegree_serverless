636b59ba96dacea91c0d08f356d07ab2be4795d1
Danielshow <danielshotonwa53@gmail.com>
2021-09-22 00:29:29 +0100

    Finish elasticsearch

--- src/lambda/dynamoDb/elasticSearchSync.ts
+++ src/lambda/dynamoDb/elasticSearchSync.ts
@@ -1,11 +1,40 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda'
 import 'source-map-support/register'
+import * as elasticsearch from 'elasticsearch'
+import * as httpAwsEs from 'http-aws-es'
+
+
+const esEndpoint = process.env.ES_ENDPOINT
+const es = new elasticsearch.Client({
+    hosts: [esEndpoint],
+    connectionClass: httpAwsEs
+})
 
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
     console.log("Processing batch events from DynamoDb: ", JSON.stringify(event))
 
     for (const record of event.Records) {
         console.log("Processing record: ", JSON.stringify(record))
-        
+        if (record.eventName == "INSERT") {
+            await processRecord(record)
+        }
     }
+}
+
+async function processRecord(record: any) {
+    const item = record.dynamodb.NewImage
+    const imageId = item.imageId.S
+    const body = {
+        imageId: imageId,
+        groupId: item.groupId.S,
+        imageUrl: item.imageUrl.S,
+        title: item.title.S,
+        timestamp: item.timestamp.S,
+    }
+    await es.index({
+        index: 'images-index',
+        type: 'images',
+        id: imageId,
+        body
+    })
 }
