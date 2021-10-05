da8ad0dd22d47231828a4f368a9dd599f1454760
Danielshow <danielshotonwa53@gmail.com>
2021-10-05 14:45:24 +0100

    Final commit

--- src/lambda/http/getImage.ts
+++ src/lambda/http/getImage.ts
@@ -40,3 +40,22 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         body: JSON.stringify(result.Items[0])
     }
 }
+
+
+// import * as express from 'express'
+// import * as awsServerlessExpress from 'aws-serverless-express'
+// const app = express()
+// app.get('/groups', async (_req, res) => {
+//     // TODO: get all groups as before
+//     const groups = ...
+  
+//     // Return a list of groups
+//     res.json({
+//       items: groups
+//     })
+//   })
+
+//   // Create Express server
+// const server = awsServerlessExpress.createServer(app)
+// // Pass API Gateway events to the Express server
+// exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
